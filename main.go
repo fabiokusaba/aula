a34b08088e4543331d85483d8d925e5c743d28fc
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-23 21:57:27 -0300

    CRUD Students com Gin

--- main.go
+++ main.go
@@ -1,7 +1,197 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Estrutura de Student
+type Student struct {
+	// Ao utilizarmos `json:"id"` estamos dizendo que o campo ID será serializado para JSON com o nome "id", desta forma estamos
+	// convertendo os campos para o formato JSON
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+// Slice de Student
+var students = []Student{
+	{ID: 1, Name: "João", Age: 20},
+	{ID: 2, Name: "Maria", Age: 22},
+	{ID: 3, Name: "José", Age: 21},
+}
+
+// Funções das rotas
+func routeHeart(c *gin.Context) {
+	// gin.H -> é um map[string]interface{}, ou seja, uma forma genérica de passar dados
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Ok",
+	})
+
+	c.Done()
+}
+
+func routeGetStudents(c *gin.Context) {
+	c.JSON(http.StatusOK, students)
+}
+
+func routeGetStudentByID(c *gin.Context) {
+	var student Student
+	// Capturando e convertendo o ID da rota para um valor inteiro
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Percorrendo a lista de students para verificar se o student com o ID informado existe
+	// Existindo o student, retornamos o student encontrado
+	// Caso não exista, retornamos um erro de "Student not found"
+	for _, s := range students {
+		if s.ID == id {
+			student = s
+		}
+	}
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Student not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
+}
+
+func routePostStudent(c *gin.Context) {
+	// Criando uma variável para do tipo Student para receber os dados vindos da requisição
+	var student Student
+
+	// Recebendo os dados da requisição e armazenando na variável student (passando o endereço da variável)
+	// Fazendo a tratativa caso ocorra algum erro
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		// Utilizamos o return para que a função finalize aqui e não continue a execução
+		return
+	}
+
+	// Incrementando o ID do novo student
+	student.ID = students[len(students)-1].ID + 1
+
+	// Adicionando o novo student a nossa listagem de students
+	students = append(students, student)
+
+	// Retornando uma resposta de sucesso
+	c.JSON(http.StatusCreated, student)
+}
+
+func routePutStudent(c *gin.Context) {
+	var studentReq Student
+	var studentFound Student
+	var newStudents []Student
+
+	if err := c.ShouldBindJSON(&studentReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Capturando e convertendo o ID da rota para um valor inteiro
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Percorrendo a lista de students para verificar se o student com o ID informado existe
+	// Existindo o student, atualizamos os dados do mesmo e retornamos o student atualizado
+	// Caso não exista, retornamos um erro de "Student not found"
+	for _, s := range students {
+		if s.ID == id {
+			studentFound = s
+		}
+	}
+
+	if studentFound.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Student not found",
+		})
+		return
+	}
+
+	studentFound.Name = studentReq.Name
+	studentFound.Age = studentReq.Age
+
+	// Atualizando a lista de students com os novos dados do student encontrado
+	for _, s := range students {
+		if s.ID == id {
+			newStudents = append(newStudents, studentFound)
+		} else {
+			newStudents = append(newStudents, s)
+		}
+	}
+
+	// Retornando uma resposta de sucesso com os dados atualizados do student
+	students = newStudents
+	c.JSON(http.StatusOK, studentFound)
+}
+
+func routeDeleteStudent(c *gin.Context) {
+	var newStudents []Student
+	// Capturando e convertendo o ID da rota para um valor inteiro
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Percorrendo a lista de students para verificar se o student com o ID informado existe
+	// Existindo o student, removemos o mesmo da lista e retornamos uma resposta de sucesso
+	for _, s := range students {
+		if s.ID == id {
+			continue
+		} else {
+			newStudents = append(newStudents, s)
+		}
+	}
+
+	students = newStudents
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Student deleted",
+	})
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	service := gin.Default()
+
+	getRoutes(service)
+
+	service.Run(":8080")
+}
+
+// Ponteiros -> trabalhando com o mesmo arquivo em memória através de sua referência, então toda vez que fizer uma modificação recebo essa
+// modificação de volta
+func getRoutes(c *gin.Engine) *gin.Engine {
+	c.GET("/heart", routeHeart)
+
+	// Criando um agrupamento de rotas
+	groupStudents := c.Group("/students")
+	groupStudents.GET("/", routeGetStudents)
+	groupStudents.GET("/:id", routeGetStudentByID)
+	groupStudents.POST("/", routePostStudent)
+	groupStudents.PUT("/:id", routePutStudent)
+	groupStudents.DELETE("/:id", routeDeleteStudent)
+
+	return c
 }
